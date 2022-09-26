4fa47962d0e796010955ce8144b3ceaab10fc7f6
Kwak, Seoungeun <kwakse0720@gmail.com>
2022-09-26 16:33:32 +0900

    최초 커밋

---
+++ src/e202014047/Hello.java
@@ -0,0 +1,8 @@
+package e202014047;
+
+public class Hello {
+
+    public void say() {
+        System.out.println("hello");
+    }
+}
---
+++ src/e202014047/Main.java
@@ -0,0 +1,9 @@
+package e202014047;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+
